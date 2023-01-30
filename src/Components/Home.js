4e8c1d8a98149bb33423b9ef5dff1889196c536c
mdaslamalidu <thehometutor2022@gmail.com>
2023-01-30 15:51:12 +0600

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="text-center my-7">
       <h1>
-        Please <Link to="/login">Log In</Link> to Add Your Bill
+        Please{" "}
+        <Link className="bg-slate-500 text-white py-2 px-3 rounded" to="/login">
+          Log In
+        </Link>{" "}
+        to Add Your Bill
       </h1>
     </div>
   );
